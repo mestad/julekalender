5d65ab9d2876ed3fd2db7ca5069000b9fd4503d2
Aase Mestad <vsxxame@vps.no>
2015-12-16 14:14:05 +0100

    luke 16 rettet

--- src/tell2Tall.groovy
+++ src/tell2Tall.groovy
@@ -5,38 +5,31 @@ import java.util.regex.Pattern
  * Created by Aase on 06.12.2015.
  */
 public class tell2Tall {
-//    public BigInteger limit = BigInteger.valueOf("12345678987654321")
-        public static String limit = "32";
+    public static String limit = "12345678987654321"
+//        public static String limit = "322";
 
 
     public static void main(String[] args) {
-        BigInteger counter = BigInteger.ZERO
-        for (int i = 0; i < limit.size(); i++) {
-            String digit = limit.substring(i, i+1)
-            int restDigits = limit.size() - i - 1
-            BigInteger restNumber = BigInteger.ZERO
-            if (restDigits > 0)
-                restNumber = new BigInteger(limit.substring(i, limit.size()))
-            BigInteger part = BigInteger.valueOf(restDigits)
-                    .multiply(BigInteger.valueOf(10).pow(restDigits))
-            if (digit.equals("1")) {
-                counter = counter.add(part)
-            } else if (digit.equals("2")) {
-                counter = counter.add(part)
-                if (restDigits > 0)
-                    counter = counter.add(restNumber)
-                else
-                    counter = counter.add(BigInteger.ONE)
-            } else {
-                if (restDigits > 0)
-                    counter = counter
-                            .add(part.multiply(BigInteger.valueOf(digit.toInteger())))
-                            .add(BigInteger.valueOf(restDigits -1)
-                            .multiply(BigInteger.valueOf(10).pow(restDigits - 1)))
-            }
 
+        println "svar: " + count2s(limit)
+    }
+
+    private static BigInteger count2s(String number) {
+        if (number.length() == 1) {
+            if (number.equals("2")) return BigInteger.ONE
+            else return BigInteger.ZERO
+        } else {
+            BigInteger toerRest = BigInteger.ZERO
+            int digit = number.substring(0,1).toInteger()
+            if (digit == 2) {
+                toerRest = new BigInteger(number.substring(1,number.length())).add(BigInteger.ONE)
+            } else if (digit > 2) {
+                toerRest = BigInteger.TEN.pow(number.length()-1)
+            }
+            return count2s(number.substring(1,number.length()))
+                            .add(toerRest)
+                            .add(BigInteger.valueOf(number.length()-1).multiply(BigInteger.valueOf(digit).multiply(BigInteger.TEN.pow(number.length()-2))))
         }
-        println "svar: " + counter
     }
 
 
@@ -45,4 +38,6 @@ public class tell2Tall {
         Matcher m = p.matcher(line)
         return m.find()
     }
+
+    //2,12,22,32, +10
 }
